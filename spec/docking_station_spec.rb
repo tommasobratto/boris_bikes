db6277a212e487d0ac02dc809678ab82a740c463
tommasobratto <tbratto22@gmail.com>
2015-02-04 16:35:26 +0000

    fixed docking_station_spec file test example

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,10 @@
 require 'docking_station'
 
 describe DockingStation do
-	let (:station)     { DockingStation.new(capacity: 20)           }
+	let (:station)     { DockingStation.new(capacity: 123)           }
 
  	it 'should allow setting default capacity on initialising' do
- 		expect(station.capacity).to eq(20)
+ 		expect(station.capacity).to eq(123)
   	end
 
 end
